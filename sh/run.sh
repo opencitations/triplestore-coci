d52d85aba318ef261f5f960232eb04ffe261a9b1
Silvio Peroni <essepuntato@gmail.com>
2018-06-11 19:10:52 +0200

    More time before block (3m)

--- sh/run.sh
+++ sh/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CUR_DIR="${pwd}"
-myv=`timeout 60s curl -s http://localhost:3001/blazegraph/sparql?query=SELECT%20%3Fs%20%7B%3Fs%20%3Fp%20%3Fo%7D%20LIMIT%201`
+myv=`timeout 180s curl -s http://localhost:3001/blazegraph/sparql?query=SELECT%20%3Fs%20%7B%3Fs%20%3Fp%20%3Fo%7D%20LIMIT%201`
 
 if [ -z "$myv" ] || [[ $myv != *"results"* ]];
 then
